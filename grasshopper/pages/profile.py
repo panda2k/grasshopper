e91a8bbf507ee3e3b11a4565e822daac722f5d7a
Kenneth Nguyen <kenneth.nguyen@nasa.gov>
2024-04-19 20:57:36 -0700

    pageRouter

---
+++ grasshopper/pages/profile.py
@@ -0,0 +1,16 @@
+import reflex as rx
+
+from dashboard.navigation import navbar
+from dashboard.template import template
+
+@template
+def profile() -> rx.Component:
+    return rx.box(
+            navbar(heading="Tools"),
+            rx.box(
+                rx.text("placeholder"),
+                margin_top="calc(50px + 2em)",
+                padding="2em",
+            ),
+            padding_left="250px",
+        )
