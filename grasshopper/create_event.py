03186dc88d9eebd40fb19e865f65ecae9f78bcee
panda2k <thelowkeypanda@gmail.com>
2024-04-20 16:37:25 -0700

    reformatted add events

--- grasshopper/create_event.py
+++ grasshopper/create_event.py
@@ -11,31 +11,43 @@ def create_event():
         rx.drawer.portal(
             rx.drawer.content(
                 rx.vstack(
-                    rx.flex(
-                        rx.heading("Create Event"),
-                        rx.drawer.close(
-                            rx.button(rx.icon("circle-x"), color_scheme="lime"),
+                    rx.box(
+                        rx.icon("chevron-down", width="8em"),
+                    ),
+                    rx.vstack(
+                        rx.box(
+                            rx.image(src="/blocks-stack.png", width="8em"),
+                            padding_top="1em",
                         ),
-                        justify="between",
+                        rx.box(
+                            rx.heading("create event", align="center"),
+                            padding="1em"
+                        ),
+                        align="center",
+                        justify="center",
                         width="100%"
                     ),
                     rx.form(
                         rx.vstack(
-                            rx.input(
-                                placeholder="Title",
-                                name="title",
-                                required=True
+                            rx.box(
+                                rx.input(
+                                    placeholder="title",
+                                    name="title",
+                                    required=True,
+                                    width="80%",
+                                ),
+                                width="100%",
                             ),
-                            rx.input(
-                                placeholder="Description",
+                            rx.text_area(
+                                placeholder="description",
                                 name="description",
-                                required=True
+                                required=True,
                             ),
                             rx.select(
-                                placeholder="School",
+                                items=GlobalState.school_names,
+                                placeholder="school",
                                 name="school",
                                 required=True,
-                                items=GlobalState.school_names,
                             ),
                             rx.input(
                                 placeholder="Location",
@@ -48,16 +60,22 @@ def create_event():
                                 name="time",
                                 min=f"{datetime.now().strftime('%Y-%m-%dT%h:%M')}"
                             ),
-                            rx.button("Create", type="submit"),
+                            rx.button("create", type="submit", variant="soft"),
+                            align="center",
+                            gap="2em",
+                            width="100%",
                         ),
                         on_submit=GlobalState.create_event,
                         reset_on_submit=True,
+                        align="center",
+                        justify="center",
                     ),
-                    width="100%"
+                    width="100%",
+                    align="center",
                 ),
                 height="90%",
                 padding="2em",
-                background_color="#FFF"
+                background_color="#FFF",
             ),
         ),
         direction="bottom"
