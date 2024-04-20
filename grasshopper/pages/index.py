2741cd34f3b4bf6c591b1465f75b4863fce666ef
Kenneth Nguyen <kenneth.nguyen@nasa.gov>
2024-04-19 22:47:46 -0700

    navBar and index/home up

--- grasshopper/pages/index.py
+++ grasshopper/pages/index.py
@@ -1,20 +1,57 @@
 """The main index page."""
 
 import reflex as rx
-from grasshopper.template import template
+from grasshopper.template import navbar, template
+
+# def eventCard(imageSrc, title, description, author, school):
+def eventCard():
+    return rx.box(
+                (rx.box(
+                rx.image(src="https://via.placeholder.com/200x150",                style={
+                    "height": "160px", 
+                    "width": "100%", 
+                    "object-fit": "cover",  
+                    "object-position": "center",
+                },
+),
+                rx.flex(
+                    rx.box(
+                        rx.heading("ACM Kickoff", as_="h1",size="5"),
+                        rx.text("ACM @ UCSD", weight="medium", trim="normal",size="4"),
+                        rx.text("UC San Diego", weight="light", trim="normal",size="3",style={"margin-bottom":'10px'}),
+                        rx.scroll_area(
+                            rx.flex(
+                                rx.text(
+                                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                                    size="2",
+                                    trim="both",
+                                ),
+                                rx.text(),
+                                type="always",
+                                scrollbars="vertical",
+                                # style={"height": "150px"},
+                        ),
+                    ),
+                    spacing="3",
+                ),
+            ),
+            as_child=True,
+        ))
+    , border_radius="10px",
+    style={"width":"100%", "padding":"10px", "margin":"10px", "box-shadow":"0 4px 8px 0 rgba(0,0,0,0.2)"})
+
+
 
 
 @template
 def index() -> rx.Component:
     return rx.center(
-        rx.theme_panel(),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.button(
-                "Check out our docs!",
-                size="4",
-            ),
-            rx.logo(),
+            rx.heading("Feed!", size="9"),
+            eventCard(),
+            eventCard(),
+            eventCard(),
+            eventCard(),
             align="center",
             spacing="7",
             font_size="2em",
--- grasshopper/template.py
+++ grasshopper/template.py
@@ -4,12 +4,35 @@ from typing import Callable
 
 import reflex as rx
 
+def navbar() -> rx.Component:
+    #create a footer with 5 elements, that link to 5 pages
+    #style it so that it is in the bottom of the page 
+    return rx.box(
+        rx.flex(
+            rx.link(rx.icon("home"), href="/"),
+            rx.link(rx.icon("calendar-clock"), href="/itinerary"),
+            rx.link(rx.icon("scan-eye"), href="/scan"),
+            # rx.link("Contact", href="/contact"),
+            rx.link(rx.icon("circle-user"), href="/profile"),
+            spacing="3",
+            justify="center",
+            align="center",
+            justify_content="space-evenly",
+        ),
+        padding="1em",
+        background_color="black",
+        color="white",
+        position="fixed",  
+        bottom="0",
+        left="0",
+        width="100%", 
 
+    )
 
 
 def template(page: Callable[[], rx.Component]) -> rx.Component:
     return rx.box(
         page(),
-        rx.logo(),
+        navbar(),
         padding_bottom="4em",
     )
