9cfbebba6baa8748e7cd56a7c6263441353970a8
panda2k <thelowkeypanda@gmail.com>
2024-04-20 18:17:54 -0700

    add terraform

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws" 
+            version = "5.31.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-west-1"
+}
+
+resource "aws_s3_bucket" "grasshopper-cdn" {
+    bucket = "grasshopper-cdn"
+}
+
+resource "aws_s3_bucket_cors_configuration" "grasshopper-cdn" {
+    bucket = aws_s3_bucket.grasshopper-cdn.id
+    
+    cors_rule {
+        allowed_headers = ["*"]
+        allowed_methods = ["PUT", "GET"]
+        allowed_origins = ["*"]
+    }
+}
